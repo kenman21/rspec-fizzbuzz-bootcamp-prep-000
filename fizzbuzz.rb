d9ea8248ce0083167625b77624ffc68a31636af7
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-01-04 20:08:20 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,11 @@
 def fizzbuzz(number)
-  if number%3 == 0 
+  if number%3 == 0 && number%5 == 0
+    puts "Fizz Buzz"
+  elsif  number%3 == 0 
     puts "Fizz"
   elsif number%5 == 0 
-    puts
+    puts "Buzz"
+  else
+    return nil 
+  end 
+end 
